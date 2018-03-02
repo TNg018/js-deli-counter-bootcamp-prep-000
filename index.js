73760708af58207c2d0deed4a40e1bd8789d0bb1
Tien Nguyen <pri859@sudomail.com>
2018-03-02 16:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ function nowServing(katzDeli) {
     }
     if (katzDeli.length === 0) {
       return "The line is currently empty.";
-    } else
-    return(`The line is currently:` + line);
+      const numbersAndNames = []
 
-  }
+     for (let i = 0, l = line.length; i < l; i++) {
+       numbersAndNames.push(`${i + 1}. ${line[i]}`)
+     }
+
+     return `The line is currently: ${numbersAndNames.join(', ')}`
+   };
